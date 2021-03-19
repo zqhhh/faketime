dae2d99a23bf097947ac358f34fd6eae3ba47c9c
zqhhh <sadeye@qq.com>
2021-03-19 23:03:01 +0800

    rename

--- faketime_test.go
+++ faketime_test.go
@@ -11,8 +11,8 @@ func TestAdd(t *testing.T) {
 	ftime.Hook()
 	defer ftime.UnHook()
 	hookTime := time.Now()
-	got := hookTime.Sub(now)
-	want := 1 * time.Hour
+	got := hookTime.Sub(now) / 1e9
+	want := 1 * time.Hour / 1e9
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
@@ -20,7 +20,15 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUnix(t *testing.T) {
-
+	var timestamp int64 = 1616165198
+	ftime := Unix(timestamp)
+	ftime.Hook()
+	defer ftime.UnHook()
+	hookTime := time.Now()
+	if hookTime.Unix() != timestamp {
+		t.Errorf("hook error,got:%d want  %d", hookTime.Unix(), timestamp)
+	}
+	t.Logf("hook time %s", hookTime)
 }
 
 func TestGetRealTime(t *testing.T) {
